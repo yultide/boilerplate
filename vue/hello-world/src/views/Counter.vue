3d8c8d379f22856347c9ed64f0b2e52f15ada694
Yulius Tjahjadi <yultide@yahoo.com>
2025-02-19 11:19:45 -0800

    add ruby on rails

---
+++ vue/hello-world/src/views/Counter.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+</script>
+
+<template>
+  <div class="">
+    <h1>Counter</h1>
+    <div>count: {{ count }}</div>
+    <button @click="increment">+</button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
